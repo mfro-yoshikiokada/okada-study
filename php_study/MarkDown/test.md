896de23493464c7bcd259963375ec7c61e665f81
mfro-yoshikiokada <135577882+mfro-yoshikiokada@users.noreply.github.com>
2023-07-03 15:16:12 +0900

    7/3

--- php_study/MarkDown/test.md
+++ php_study/MarkDown/test.md
@@ -42,3 +42,8 @@ int i = 0; //コード
 
 例： ![qiita-square.png](https://qiita-image-store.s3.amazonaws.com/0/126861/90386757-fd96-8ba6-3477-485669713c55.png "qiita-square")
 ***
+1. First list item
+    - First nested list item
+        - Second nested list item
+      - 
+       - [x] Second nested list item
--- php_study/practice_11/index.php
+++ php_study/practice_11/index.php
@@ -42,23 +42,23 @@ $subject = str_replace($search, $replace, $subject);
 echo $subject;
 //実行結果：<h1 id="h1-class" class="h1-id">これはh1タグの中の文字列です</h1>
 
-//放送禁止用語の削除
+//放送禁止用語の削除 1
 $forbiddenWords = array("fucking", "fuck", "shit","bitch");
 $message = "It’s fucking boring.Eat shit and die.";
 
 $filteredMessage = str_replace($forbiddenWords, "****", $message);
 
 echo "Filtered Message: " . $filteredMessage . "\n";
-//URL文字をリンクにする
+//URL文字をリンクにする 2
 $val = '当サイトのURLは https://nagablog.info/ です。';
 $urlPattern = '/https?(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)/';
 echo preg_replace($urlPattern, '<a href="\\0">\\0</a>', $val);
 
-// 改行コードをbrタグへ置換
+// 改行コードをbrタグへ置換 3
 $val = 'バナナは南国の食べ物です。
 色は黄色で美味しいです。';
 echo preg_replace('/\n/', '<br>', $val);
-//ファイルの名前を置き換え
+//ファイルの名前を置き換え 4
 $filePath = "/path/to/file.txt";
 
 $pattern = '/(.*)\/([^\/]+)\.\w+$/';
@@ -69,20 +69,20 @@ $newFilePath = preg_replace($pattern, $replacement, $filePath);
 echo "New File Path: " . $newFilePath . "\n";
 
 
+//5 複数対象を置換する
 $text = 'apple100 banana150';
 
-
-$pattern = ['/[0-9]{3}/'];
+$pattern = ['/[0-9]{3}/', '/apple/', '/banana/'];
 $replace = ['', 'apple is red. ', 'banana is yellow.'];
 $result = preg_replace($pattern, $replace, $text);
-echo $result; // apple banana
-
+echo $result; // apple is red.  banana is yellow.
+echo("<br>");
 $str = 'ABCDEFGHIJKL1234567';
-//アルファベットA-Zを全て'Z'に置き換える
+//アルファベットA-Zを全て'Z'に置き換える 6
 $str_grep = mb_ereg_replace('[A-Z]', 'Z', $str);
 
 echo $str_grep;
-//配列の場合
+//配列の場合 7
 $texts = ['apple100 banana150', 'apple apple apple', '700円のビール, 100円のapple'];
 $pattern = ['/[0-9]{3}/', '/apple/', '/banana/'];
 $replace = ['', 'apple is red. ', 'banana is yellow.'];
@@ -90,27 +90,28 @@ $replace = ['', 'apple is red. ', 'banana is yellow.'];
 $result = preg_replace($pattern, $replace, $texts);
 var_dump($result);
 
-//置換対応の配列を使って変換する
+//置換対応の配列を使って変換する 8
 $text = 'apple banana orange grape';
 $trans = ['apple' => 'red', 'banana' => 'yellow', 'e' => "e!!"];
 echo strtr($text, $trans); // red yellow orange!! grape!!
-
+echo("<br>");
 // CSVデータ
 $csvData = 'John,Doe,JohnDoe@example.com
 Jane,Smith,JaneSmith@example.com
 Alex,Johnson,AlexJohnson@example.com';
 
-// 改行文字をカンマに置換してCSVデータを整形
-$csvData = preg_replace("/\r\n|\n|\r/", ",", $csvData);
 var_dump($csvData);
+echo("<br>");
+// 改行文字をカンマに置換してCSVデータを整形 9
+$csvData = preg_replace("/(\r\n|\n|\r)/", ",", $csvData);
+var_dump($csvData);
+echo("<br>");
 
-// 特定の文字列の出現回数をカウントする
-$text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae ipsum ipsum, sed eu ipsum. Ipsum dolor ipsum.";
 
-// カウントする文字列
-$targetString = "ipsum";
+//URLのパラメータを書き換える 10
+$url = "https://www.xxxx.com/?word=dryer";
+$change = "/?word=change";
+echo("<br>");
 
-$count = preg_match_all("/" . preg_quote($targetString, '/') . "/", $text, $matches);
-
-echo "Target String: $targetString\n";
-echo "Count: $count\n";
+$csvData = preg_replace("/\/\?[a-z=]+/", $change, $url);
+var_dump($csvData);
