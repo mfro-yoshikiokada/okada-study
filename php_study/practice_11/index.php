589238e43e813f528c9676f41cc8e475c3753a25
mfro-yoshikiokada <135577882+mfro-yoshikiokada@users.noreply.github.com>
2023-07-19 17:17:04 +0900

    addReplace

--- php_study/practice_11/index.php
+++ php_study/practice_11/index.php
@@ -95,6 +95,7 @@ $text = 'apple banana orange grape';
 $trans = ['apple' => 'red', 'banana' => 'yellow', 'e' => "e!!"];
 echo strtr($text, $trans); // red yellow orange!! grape!!
 echo("<br>");
+
 // CSVデータ
 $csvData = 'John,Doe,JohnDoe@example.com
 Jane,Smith,JaneSmith@example.com
@@ -102,39 +103,78 @@ Alex,Johnson,AlexJohnson@example.com';
 
 var_dump($csvData);
 echo("<br>");
-// 改行文字をカンマに置換してCSVデータを整形 9
+// 改行文字をカンマに置換してCSVデータを整形 1
 $csvData = preg_replace("/(\r\n|\n|\r)/", ",", $csvData);
 var_dump($csvData);
 echo("<br>");
 
 
-//URLのパラメータを書き換える 10
+//URLのパラメータを書き換える 2
 $url = "https://www.xxxx.com/?word=dryer";
 $change = "/?word=change";
 echo("<br>");
 
-//メアドの@以降を取っ払う 11
+//メアドの@以降を取っ払う 3
 $email = "example@example.com";
 $maskedEmail = preg_replace("/@.*/", "", $email);
 echo "置き換え後のメールアドレス: $maskedEmail\n";
 
-//CSVを文字列にして空白に置き換える 12
+//CSVを文字列にして空白に置き換える 4
 $csvData = 'John,Doe,JohnDoe@example.com';
 
 $replacedData = str_replace(',', ' ', $csvData);
 
 echo " $replacedData\n";
-//htmlでpタグをstrongタグに変更　13
+//htmlでpタグをstrongタグに変更　5
 $html = "<p>Hello, <strong>World!</strong></p>";
 $plainText = str_replace(["<p>", "</p>", "<strong>", "</strong>"], "", $html);
 echo " $plainText\n";
 
-//hellowをHi!におきかえる 14
+//hellowをHi!におきかえる 6
 $text = "Hello, world! How are you?";
 $newText = str_replace("hello", "Hi!", $text);
 echo " $newText\n";
 
-//IPアドレスを伏せる 15
+//IPアドレスを伏せる 7
 $text = "The IP address is 192.168.0.1";
 $maskedText = preg_replace("/\b(\d{1,3}\.\d{1,3}\.\d{1,3})\.\d{1,3}\b/", "***.***.***", $text);
+echo " $maskedText\n";
+
+
+//特定の文字列の後ろの桁数を０にする 8
+$string = "あいうえお12345";
+$maskedText = preg_replace( "/(\d+)$/","0",  $string);
+echo " $maskedText\n";
+$string = "あいうえお12345";
+
+//金額の数字のみを伏字にする 9
+ $target = 'This water(100ml) is 100yen';
+
+$maskedText = preg_replace( "/\d3(?=yen)/","xxxx",  $target);
+echo " $maskedText\n";
+
+//2桁と３桁の数字のみ取得し伏字にする。10
+$target = '150 is greater than 15.';
+
+$maskedText = preg_replace( " /\d{2,3}/","xxx",  $target);
+echo " $maskedText\n";
+
+//1と１２の繰り返す部分のみ伏字 11
+$target = '1, 112, 11212, 1121212';
+$maskedText = preg_replace( "/1(12)*/","xxx",  $target);
+echo " $maskedText\n";
+
+//一番目のcatのみをdogに変更 12
+$target = 'cat dog cat';
+$maskedText = preg_replace( "/^cat/","dog",  $target);
+echo " $maskedText\n";
+
+//JavaScript Javaを伏字にする 13
+const target = 'Python JavaScript Java';
+$maskedText = preg_replace( "/Java(Script)?/","xxxxxx",  $target);
+echo " $maskedText\n";
+
+// This is React, This is JavaScript をThis is the Python に変更 14
+const target = 'This is React This is JavaScript';
+$maskedText = preg_replace( "/This is (React|JavaScript)/","This is the Python",  $target);
 echo " $maskedText\n";
