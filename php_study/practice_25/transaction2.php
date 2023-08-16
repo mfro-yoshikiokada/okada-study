f6cd8838cd518991b99fc2957904556684c77295
mfro-yoshikiokada <135577882+mfro-yoshikiokada@users.noreply.github.com>
2023-08-16 18:11:38 +0900

    sql2

--- php_study/practice_25/transaction2.php
+++ php_study/practice_25/transaction2.php
@@ -14,10 +14,16 @@ if ($conn->connect_error) {
 $conn->begin_transaction();
 
 try {
-    $sql1 = "UPDATE list SET age = 0, name = 'john smith' WHERE 1";
+
+    $sql1 = "UPDATE list SET age = 1, name = 'john' WHERE 1";
 
     if (!$conn->query($sql1)) {
         throw new Exception("sql1 failed.");
+    }
+    $sql2 = "UPDATE  SET age = 0, name = 'john smith' WHERE 1";
+
+    if (!$conn->query($sql2)) {
+        throw new Exception("sql2 failed.");
     }
 
     // トランザクションをコミット
@@ -32,3 +38,8 @@ try {
 
     $conn->close();
 }
+/*
+ * 挙動確認：
+ * 条件分岐で何もせずに命令文を出すと何もない状態でerrorも出ずそのまま実行されできないものは無視される。
+ * 条件分岐をするとerrorが出てrollbackすることで前行った処理も行う前の状態に戻る。
+ */
