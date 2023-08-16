18c355d193ff59c85a8e4266a9816f10bf2cb4d4
mfro-yoshikiokada <135577882+mfro-yoshikiokada@users.noreply.github.com>
2023-08-16 17:59:08 +0900

    sql1

--- php_study/practice_25/transaction2.php
+++ php_study/practice_25/transaction2.php
@@ -14,13 +14,14 @@ if ($conn->connect_error) {
 $conn->begin_transaction();
 
 try {
-    $sql1 = "UPDATE list SET age = 25, name = 'example' WHERE id = 1";
-    $conn->query($sql1);
+    $sql1 = "UPDATE list SET age = 0, name = 'john smith' WHERE 1";
 
-    $sql2 = "UPDATE list SET age = '257897897895456745456546', name = example WHERE id = 1";
-    $conn->query($sql2);
+    if (!$conn->query($sql1)) {
+        throw new Exception("sql1 failed.");
+    }
 
-
+    // トランザクションをコミット
+    $conn->commit();
     $conn->commit();
     echo "Transaction committed successfully.";
 } catch (Exception $e) {
