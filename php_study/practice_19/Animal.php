8d0081e8743ac543f24dc7c8c6b58d2262ae54df
mfro-yoshikiokada <135577882+mfro-yoshikiokada@users.noreply.github.com>
2023-09-06 11:18:10 +0900

    名前空間適用できない

--- php_study/practice_19/Animal.php
+++ php_study/practice_19/Animal.php
@@ -1,5 +1,5 @@
+
 <?php
-// Animal.php
 
 namespace MyNamespace;
 
--- php_study/practice_19/Dog.php
+++ php_study/practice_19/Dog.php
@@ -1,16 +1,18 @@
+
 <?php
 
 namespace MyNamespace;
+require_once 'Animal.php';
 
 class Dog extends AbstractAnimal
 {
-public function getName(): string
-{
-return "Dog";
-}
+    protected function getName(): string
+    {
+        return "Dog";
+    }
 
-public function makeSound(): string
-{
-return "Woof! Woof!";
-}
+    protected function makeSound(): string
+    {
+        return "Woof!";
+    }
 }
