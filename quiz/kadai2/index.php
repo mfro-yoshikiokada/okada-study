07a70b059a34f29e97d32bafd54ae3944f7b04c8
mfro-yoshikiokada <135577882+mfro-yoshikiokada@users.noreply.github.com>
2023-09-27 12:24:58 +0900

    kadai2-3

--- quiz/kadai2/index.php
+++ quiz/kadai2/index.php
@@ -7,43 +7,32 @@ class Brave {
     public $hp;
     public $power;
 
-    public function __construct($name, $hp , $power) {
+    public function __construct($name, $hp, $power) {
         $this->name = $name;
         $this->hp = $hp;
         $this->power = $power;
     }
 
-    /**
-     * @return mixed
-     */
-    public function getName()
-    {
+    public function getName() {
         return $this->name;
     }
 
-
-    /**
-     * @return mixed
-     */
-    public function getHp()
-    {
+    public function getHp() {
         return $this->hp;
     }
 
-    /**
-     * @return mixed
-     */
-    public function getPower()
-    {
+    public function getPower() {
         return $this->power;
     }
 
-    public function attack () {
+    public function attack() {
         echo "こうげきをした！";
     }
 }
+
 $c = new Brave('異世界転生マン', '1000', '500');
 echo("私の名前は".$c->getName()."\n");
 echo("HPは".$c->getHp()."\n");
 echo("力は".$c->getPower()."\n");
 
+?>
