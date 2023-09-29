87628ea058af201973123b444e01b2693ebcceba
mfro-yoshikiokada <135577882+mfro-yoshikiokada@users.noreply.github.com>
2023-09-29 16:02:52 +0900

    kadai2-10

--- quiz/kadai2/index.php
+++ quiz/kadai2/index.php
@@ -48,7 +48,7 @@ class Brave
 }
 
 $c = new Brave('異世界転生マン', '1000', '500');
-if ($c->getActive())  {
+if ($c->getActive()) {
     echo("私の名前は".$c->getName()."\n");
     echo("HPは".$c->getHp()."\n");
     echo("力は".$c->getPower()."\n");
