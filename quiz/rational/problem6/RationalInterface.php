db2e6553c4b8d803876d1b145315dcc11364a230
mfro-yoshikiokada <135577882+mfro-yoshikiokada@users.noreply.github.com>
2023-11-01 15:57:53 +0900

    諸々改善4

--- quiz/rational/problem6/RationalInterface.php
+++ quiz/rational/problem6/RationalInterface.php
@@ -5,6 +5,6 @@ interface RationalInterface
 {
     public function display();
     public function number();
-    public function add(Rational|MutableRational $other);
-    public function sub(Rational|MutableRational$other);
+    public function add(Rational|MutableRational $other):Rational|MutableRational;
+    public function sub(Rational|MutableRational $other):Rational|MutableRational;
 }
