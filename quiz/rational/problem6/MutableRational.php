8759e9e34f4b8cec0f3a55ac5afb36a30daec27c
mfro-yoshikiokada <135577882+mfro-yoshikiokada@users.noreply.github.com>
2023-11-06 12:01:25 +0900

    12

---
+++ quiz/rational/problem6/MutableRational.php
@@ -0,0 +1,63 @@
+<?php
+namespace Rational\problem6;
+
+class MutableRational implements RationalInterface
+{
+    public int $numerator;
+    public int $denominator;
+
+    public function __construct(int $numerator, int $denominator)
+    {
+        $this->numerator= $numerator;
+        $this->denominator=$denominator;
+    }
+    private function approx(int $numerator, int $denominator):array
+    {
+        $originalNumerator=$numerator;
+        $originalDenominator=$denominator;
+        $remainder = 1;
+        $newNumerator= 0;
+        $newDenominator = 0;
+        while ($remainder != 0) {
+            $remainder = $denominator% $numerator;
+            if ($remainder==0) {
+                $greatestCommonDivisor=$numerator;
+                $newNumerator= $originalNumerator/$greatestCommonDivisor;
+                $newDenominator= $originalDenominator/$greatestCommonDivisor;
+            } else {
+                $denominator=$numerator;
+                $numerator=$remainder;
+            }
+        }
+        return [$newNumerator,$newDenominator];
+    }
+    public function number():int
+    {
+        return $this->numerator % $this->denominator;
+    }
+
+    public function display():string
+    {
+        return "$this->numerator / $this->denominator";
+    }
+
+    public function add(RationalInterface $other): RationalInterface
+    {
+        $denominatorMultiplied = $this->denominator * $other->denominator;
+        $moleculeMultiplied = $this->denominator * $other->numerator + $other->denominator * $this->numerator;
+        $result= $this->approx($moleculeMultiplied, $denominatorMultiplied);
+        $this->numerator= $result[0];
+        $this->denominator=$result[1];
+        return $this;
+    }
+
+    public function sub(RationalInterface $other): RationalInterface
+    {
+        $denominatorMultiplied = $this->denominator * $other->denominator;
+        $moleculeMultiplied = $this->denominator * $other->numerator - $other->denominator * $this->numerator;
+        $result= $this->approx($moleculeMultiplied, $denominatorMultiplied);
+        $this->numerator = $result[0];
+        $this->denominator = $result[1];
+        return $this;
+    }
+}
