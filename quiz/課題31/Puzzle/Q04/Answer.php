33a3f9beac95c7d44d7bfe024f40168f8bc3cb06
mfro-yoshikiokada <135577882+mfro-yoshikiokada@users.noreply.github.com>
2023-11-08 12:18:41 +0900

    Q04 2

--- quiz/課題31/Puzzle/Q04/Answer.php
+++ quiz/課題31/Puzzle/Q04/Answer.php
@@ -7,31 +7,15 @@ class Answer
     public function exec($stickSize, $people)
     {
         $cutTimes = $stickSize - 1 ;
-        $cutNum= [1];
+        $cutNum = [1];
         $result = 0;
         for ($i = 0; $i <= $cutTimes; $i++) {
-
             array_push($cutNum,$cutNum[$i]*2);
         }
-        echo "<br/>";
-        var_dump($cutNum);
-        $whileCount=0;
-        while ($cutTimes>=0) {
-            echo "<br/>";
-            echo "whileCount";
-            echo "$whileCount";
+        $whileCount = 0;
+        while ($cutTimes >= 0) {
             if ($cutNum[$whileCount] >= $people) {
-                echo "<br/>";
-                echo "カットできる人数";
-                echo "$cutNum[$whileCount]";
-
-                echo "<br/>";
-                echo "人数";
-                echo "$people";
                 $cutTimes = $cutTimes-$people;
-                echo "<br/>";
-                echo "残りカット数";
-                echo "$cutTimes";
                 $result++;
             } else {
                 $cutTimes = $cutTimes - $cutNum[$whileCount];
