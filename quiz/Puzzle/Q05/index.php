9f35b07340e7329194e2aaf21cc2a2988b0cecac
mfro-yoshikiokada <135577882+mfro-yoshikiokada@users.noreply.github.com>
2023-11-15 15:59:28 +0900

    効率化2-5

--- quiz/Puzzle/Q05/index.php
+++ quiz/Puzzle/Q05/index.php
@@ -6,9 +6,5 @@ use Quiz\Puzzle\Q05\AnswerInterface;
 use Quiz\Puzzle\Q05\Answer;
 
 $answer= new Answer();
-$answer->setCoinTypes([500, 100, 50, 10,5,1]) ;
-$time_start = microtime(true);
+$answer->setCoinTypes([500, 100, 50, 10,5,1]);
 var_dump($answer->exec(1000));
-$time_end = microtime(true);
-$time = $time_end - $time_start;
-var_dump($time); //実行時間を出力する
