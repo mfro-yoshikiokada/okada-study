f1f859afc1b0bc3d80c36845617c638d86573f7e
mfro-Inoue <torata.inoue@mfro.net>
2023-11-22 16:27:24 +0900

    コインの最大枚数の追加

--- quiz/Puzzle/Q05/Answer2.php
+++ quiz/Puzzle/Q05/Answer2.php
@@ -7,8 +7,11 @@ class Answer2 implements AnswerInterface
 {
     const COIN_500 = 500;
 
+    private int $maxCoins;
+
     public function __construct()
     {
+        $this->maxCoins = 15;
     }
 
     public function exec(int $bill): int
