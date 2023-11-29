f8f5918b96c010cbbe3fdc6c2293aabb8735a15b
mfro-yoshikiokada <135577882+mfro-yoshikiokada@users.noreply.github.com>
2023-11-29 16:35:33 +0900

    COIN_100 9

--- quiz/Puzzle/Q05/Answer2.php
+++ quiz/Puzzle/Q05/Answer2.php
@@ -21,33 +21,41 @@ class Answer2 implements AnswerInterface
     {
         $total =0;
         $result=0;
+        $coinCount = 0;
         $numberOfLoops500 =$bill / self::COIN_500;
         $numberOfLoops100 =$bill / self::COIN_100;
-        $numberOfLoops50 =$bill / self::COIN_50;
-        $numberOfLoops10 =$bill / self::COIN_10;
+        $numberOfLoops50 =$bill / self::COIN_50+1;
+        $numberOfLoops10 =$bill / self::COIN_10+1;
         for ($count = -1; $count < $numberOfLoops500 && $total <= $bill; $count++) {
             $cnt100=0;
-            for ($count100 = -1; $count100 < $numberOfLoops100 && $total <= $bill; $count100++) {
+            for ($count100 = -1; $count100 < $numberOfLoops100 && $total <= $bill && $coinCount-1 <$this->maxCoins; $count100++) {
                 $cnt50=0;
                 $cnt100++;
-                for ($count50 = -1; $count50 < $numberOfLoops50 && $total <= $bill; $count50++) {
+                for ($count50 = -1; $count50 < $numberOfLoops50 && $total <= $bill && $coinCount-1 <$this->maxCoins; $count50++) {
                     $cnt10=0;
                     $cnt50++;
-                    for ($count10 = -1; $count10 < $numberOfLoops10 && $total <= $bill; $count10++) {
+                    for ($count10 = -2; $count10 < $numberOfLoops10 && $total <= $bill && $coinCount-1 <$this->maxCoins; $count10++) {
                         $cnt10++;
                         if ($bill ==$total) {
                             $result++;
                         }
                         $total = $total+ self::COIN_10;
+                        $coinCount++;
                     }
+                    $coinCount = $coinCount-$cnt10;
                     $total = $total - self::COIN_10*$cnt10;
                     $total = $total + self::COIN_50;
+                    $coinCount++;
                 }
+                $coinCount = $coinCount-$cnt50;
                 $total = $total - self::COIN_50*$cnt50;
                 $total =  $total +self::COIN_100;
+                $coinCount++;
             }
+            $coinCount = $coinCount-$cnt100;
             $total = $total - self::COIN_100*$cnt100;
             $total =  $total + self::COIN_500;
+            $coinCount++;
         }
         return $result;
     }
--- quiz/Puzzle/Q05/index.php
+++ quiz/Puzzle/Q05/index.php
@@ -1,9 +1,11 @@
 <?php
 require_once 'AnswerInterface.php';
-require_once 'Answer2.php';
+require_once 'Answer.php';
 
 use Quiz\Puzzle\Q05\AnswerInterface;
-use Quiz\Puzzle\Q05\Answer2;
+use Quiz\Puzzle\Q05\Answer;
 
-$answer= new Answer2();
+$answer= new Answer();
+$answer->setCoinTypes([10, 50, 100, 500]) ;
+
 var_dump($answer->exec(500));
