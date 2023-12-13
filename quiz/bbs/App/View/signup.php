1c91de3d00f57a2120a714234d6a474f2651b288
mfro-yoshikiokada <135577882+mfro-yoshikiokada@users.noreply.github.com>
2023-12-13 16:21:52 +0900

    CMPT_users 3

--- quiz/bbs/App/View/signup.php
+++ quiz/bbs/App/View/signup.php
@@ -10,7 +10,11 @@ header("Pragma:no-cache");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+          crossorigin="anonymous"
+    >
 </head>
 <header>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
@@ -22,7 +26,9 @@ header("Pragma:no-cache");
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/quiz/bbs/public/users/login.php">login</a>
+                        <a class="nav-link active" aria-current="page"
+                           href="/quiz/bbs/public/users/login.php"
+                        >login</a>
                     </li>
             </div>
         </div>
@@ -38,5 +44,8 @@ header("Pragma:no-cache");
 </form>
 
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"
+></script>
 </html>
