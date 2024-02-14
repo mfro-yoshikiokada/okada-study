23ad232bf7c355aaffd7687449d3ee0ef37272b3
mfro-yoshikiokada <135577882+mfro-yoshikiokada@users.noreply.github.com>
2024-02-14 11:34:49 +0900

    404 not      found

--- laravel-tutorial/resources/views/myCart.blade.php
+++ laravel-tutorial/resources/views/myCart.blade.php
@@ -1,15 +1,14 @@
 <x-app-layout>
     <div class="container-fluid">
         <div class="mx-auto" style="max-width:1200px">
-            <h1 style="color:#555555; text-align:center; font-size:1.2em; padding:24px 0px; font-weight:bold;">商品一覧</h1>
+            <h1 style="color:#555555; text-align:center; font-size:1.2em; padding:24px 0px; font-weight:bold;">
+                {{ Auth::user()->name }}さんのカートの中身</h1>
+            <p class="text-center">{{ $message ?? '' }}</p><br>
             <div class="">
-                <div class="">
-
-                    @foreach($myCartStocks as $stock)
-                        {{$stock->stockId}}<br>
-                        {{$stock->userId}}<br>
-                    @endforeach
-                </div>
+                @foreach($myCartStocks as $stock)
+                    {{$stock->stockId}}<br>
+                    {{$stock->userId}}<br>
+                @endforeach
             </div>
         </div>
     </div>
--- laravel-tutorial/resources/views/stocks.blade.php
+++ laravel-tutorial/resources/views/stocks.blade.php
@@ -4,7 +4,6 @@
             <h1 style="color:#555555; text-align:center; font-size:1.2em; padding:24px 0px; font-weight:bold;">商品一覧</h1>
             <div class="">
                 <div class="grid grid-cols-4 gap-4 flex-wrap">
-                    {{-- 追加 --}}
                     @foreach($stocks as $stock)
                         <div class="mycart_box text-center rounded shadow-lg bg-white p-6">
                             {{$stock->name}} <br>
@@ -12,24 +11,19 @@
                             <img src="/image/{{$stock->imagePath}}" alt="" class="incart" >
                             <br>
                             {{$stock->explain}} <br>
+
+                            <form action="addMyCart" method="post">
+                                @csrf
+                                <input type="hidden" name="stockId" value="{{ $stock->id }}">
+                                <input type="submit" value="カートに入れる">
+                            </form>
                         </div>
-                        <form action="addMyCart" method="post">
-                            @csrf
-                            <input type="hidden" name="stockId" value="{{ $stock->id }}">
-                            <input type="submit" value="カートに入れる">
-                        </form>
+
                     @endforeach
-                    {{-- ここまで --}}
                 </div>
                 <div class="text-center" style="width: 200px;margin: 20px auto;">
                     {{  $stocks->links()}}
                 </div>
-                </div>
-                {{-- 追加 --}}
-                <div class="text-center" style="width: 200px;margin: 20px auto;">
-                    {{  $stocks->links()}}
-                </div>
-                {{-- ここまで --}}
             </div>
         </div>
     </div>
