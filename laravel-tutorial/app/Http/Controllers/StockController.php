29e4d3714c9aa3c1a4636e4b46f3fe2ee3ad39f0
mfro-yoshikiokada <135577882+mfro-yoshikiokada@users.noreply.github.com>
2024-02-22 16:11:22 +0900

    add stock3

--- laravel-tutorial/app/Http/Controllers/StockController.php
+++ laravel-tutorial/app/Http/Controllers/StockController.php
@@ -11,7 +11,7 @@ class StockController extends Controller
 {
     public function index()
     {
-        $stocks = Stock::SimplePaginate(6); //Eloquantで検索
+        $stocks = Stock::SimplePaginate(20); //Eloquantで検索
         return view('stocks', ['stocks' => $stocks]);
     }
 
@@ -20,30 +20,28 @@ class StockController extends Controller
         $myCartStocks = $userStock->showMyCart();
         return view('myCart',compact('myCartStocks'));
     }
+
     public function addPage()
     {
-        $img_name = realpath("image");
-        return view('addStock', ['img_name' => $img_name]);
+        return view('addStock');
     }
+
     public function addStock(Request $request)
     {
-        $name = $request->input('name');
-        $explanation = $request->input('explanation');
-        $fee = (int) $request->input('fee');
         $str = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPUQRSTUVWXYZ';
         $img_name = substr(str_shuffle($str), 0, 10);
         $image_directory = "/var/www/html/laravel-tutorial/public/image/";
         $tmp_file_path = $_FILES["file"]["tmp_name"];
         $destination_path = $image_directory . $img_name . '.jpeg';
-        Stock::create([
-            'name' => $name,
-            'explain' => $explanation,
-            'fee' => $fee,
+        $insert = [
+            'name' => $request->input('name'),
+            'explain' => $request->input('explanation'),
+            'fee' => (int) $request->input('fee'),
             'imagePath' => $img_name . '.jpeg'
-        ]);
+        ];
+        Stock::create($insert);
         move_uploaded_file($tmp_file_path, $destination_path);
-        header("Location:laravel-tutorial/public/");
-        exit;
+        return redirect('/');
     }
 
     public function addMycart(Request $request,UserStock $userStock)
