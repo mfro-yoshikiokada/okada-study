080161e69f4ce779a16b408cd5efe8114ddd1698
mfro-yoshikiokada <135577882+mfro-yoshikiokada@users.noreply.github.com>
2024-04-09 17:47:41 +0900

    changed Validation

--- laravel-tutorial/app/Http/Requests/ArticleStoreRequest.php
+++ laravel-tutorial/app/Http/Requests/ArticleStoreRequest.php
@@ -23,11 +23,10 @@ class ArticleStoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', new MaxWordCountValidation(10)],
-            'fee' => ['required', 'max:8'],
-            'explanation' => ['required', new MaxWordCountValidation(200)],
+            'name' => ['required', 'max:10'],
+            'fee' => ['required', 'numeric','max:8'],
+            'explanation' => ['required', 'max:200'],
             'file' => 'required',
-            'sub-files.*.photo' => 'image|mimes:jpeg,bmp,png',
         ];
     }
     public function attributes()
@@ -37,6 +36,26 @@ class ArticleStoreRequest extends FormRequest
             "fee" => "値段",
             "explanation" => "説明欄",
             'file' => "写真"
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages(): array
+    {
+        return [
+            'name.required' => '名前を入力してください',
+            'name.max' => '名前は10文字以内で入力してください',
+            'explanation.required' => '説明欄を入力してください',
+            'explanation.max' => '説明欄は200文字未満で入力してください',
+            'fee.required' => '値段を入力してください',
+            'fee.numeric' => '値段は数字で入力してください',
+            'fee.max' => '値段は1から8桁の数字で入力してください',
+            'fee.min' => '値段は1以上を入力してください',
+            'file.required' => '写真を入力してください'
         ];
     }
 }
