b909db6633f15570c870223e43aec31a2da455e3
mfro-yoshikiokada <135577882+mfro-yoshikiokada@users.noreply.github.com>
2024-04-11 17:07:07 +0900

    threads.php

--- quiz/bbs/App/View/threads.php
+++ quiz/bbs/App/View/threads.php
@@ -1,6 +1,6 @@
 <?php session_start();
 header("Cache-Control:no-cache,no-store,must-revalidate,max-age=0");
-header("Cache-Control:pre-check=0","post-check=0",false);
+header("Cache-Control:pre-check=0", "post-check=0", false);
 header("Pragma:no-cache");
 ?>
 
@@ -10,13 +10,26 @@ header("Pragma:no-cache");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link 
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+    rel="stylesheet" 
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
+    crossorigin="anonymous"
+    >
 </head>
 <header>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button 
+    class="navbar-toggler"
+    type="button" 
+    data-bs-toggle="collapse" 
+    data-bs-target="#navbarSupportedContent" 
+    aria-controls="navbarSupportedContent" 
+    aria-expanded="false" 
+    aria-label="Toggle navigation"
+    >
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -57,8 +70,13 @@ header("Pragma:no-cache");
 
     var_dump($_SESSION);
 echo '<ul>';
-foreach ($comments as $pref){
-    echo '<li><p>id:'.$pref ["user_id"].'</p><p>'.$pref ["comment_text"].'</p><p>time:'.$pref ["created_at"].'</p></li>';
+foreach ($comments as $pref) {
+    echo 
+    '<li>
+      <p>id:'.$pref ["user_id"].'</p>
+      <p>'.$pref ["comment_text"].'</p>
+      <p>time:'.$pref ["created_at"].'</p>
+    </li>';
 }
 echo '</ul>';
 ?>
@@ -68,6 +86,10 @@ echo '</ul>';
     <button type="submit">送信</button>
 </form>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script 
+src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" 
+crossorigin="anonymous"
+></script>
 </html>
 
